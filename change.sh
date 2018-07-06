2f3da8d644bfedea8b1b11b543d4d28308a2f2eb
Ewige前夕-Logic <Master@evec.cc>
2018-07-07 00:27:52 +0800

    Update change.sh

--- change.sh
+++ change.sh
@@ -8,13 +8,13 @@ Stack=$1
 
 case "${Stack}" in
     Open)
-        wget -q https://raw.githubusercontent.com/EVECloud/EPDomainCharge/master/domain.ctl.php00 -O domain.ctl.php
+        wget -q https://raw.githubusercontent.com/EVECloud/EPDomainChange/master/domain.ctl.php00 -O domain.ctl.php
 	    mv -f domain.ctl.php /vhs/kangle/nodewww/webftp/vhost/control/domain.ctl.php
 	    rm -rf domain.ctl.php
 		echo -e "前台用户泛解析功能:开"
         ;;
     Close)
-        wget -q https://raw.githubusercontent.com/EVECloud/EPDomainCharge/master/domain.ctl.php11 -O domain.ctl.php
+        wget -q https://raw.githubusercontent.com/EVECloud/EPDomainChange/master/domain.ctl.php11 -O domain.ctl.php
 	    mv -f domain.ctl.php /vhs/kangle/nodewww/webftp/vhost/control/domain.ctl.php
 	    rm -rf domain.ctl.php
 		echo -e "前台用户泛解析功能:关"
